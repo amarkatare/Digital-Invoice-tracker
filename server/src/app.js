82ca134645803a7affd245dd17938050200c2b2f
ShingareShivraj <sssyelur@gmail.com>
2025-11-13 11:04:14 +0530

    first

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is working...");
+});
+
+export default app;
